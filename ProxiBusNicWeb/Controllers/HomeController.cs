77e771800bf0fe7834861bd1b37e1aa1e7df1db5
Tyler Rodríguez <tallo.rodriguez.perez@gmail.com>
2022-07-10 00:00:41 -0600

    Preparación de app para implementar el login

--- ProxiBusNicWeb/Controllers/HomeController.cs
+++ ProxiBusNicWeb/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace ProxiBusNicWeb.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private ProxiBusNicEntityContainer db = new ProxiBusNicEntityContainer();
