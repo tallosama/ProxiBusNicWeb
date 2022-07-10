2ede000cd46810d86057f97fb2dd6a46f3f28f33
Tyler Rodríguez <tallo.rodriguez.perez@gmail.com>
2022-07-09 22:54:28 -0600

    Implementación de datatables

---
+++ ProxiBusNicWeb/Content/vendor/datatables/DataTables.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#Tabla').DataTable({
+        responsive: true,
+        autoWidth: false,
+
+
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros por página",
+            "zeroRecords": "No hemos encontrado nada - disculpe :'(",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay ningún registro disponible",
+            "infoFiltered": "(Filtrado de _MAX_ registros)",
+            "search": "Buscar:",
+            "paginate": {
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        }
+
+
+    }
+    );
+});
